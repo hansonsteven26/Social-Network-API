dd5aa4ba49b280c29f1df5f0a943cd06b2a86879
steven <smhanson21@gmail.com>
2023-11-13 17:44:21 -0600

    create controller/route files

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -5,7 +5,12 @@ const thoughtSchema = new Schema(
     thoughtText: {
       type: String,
       required: true,
-      min
+      minLength: 1,
+      maxLength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now()
     }
   }
 )
