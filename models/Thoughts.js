ec02b1802a92daacd1d5a84e8465e0df3083cca6
steven <smhanson21@gmail.com>
2023-11-13 18:25:34 -0600

    finish createdAt & custom methods

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -6,11 +6,32 @@ const thoughtSchema = new Schema(
       type: String,
       required: true,
       minLength: 1,
-      maxLength: 280
+      maxLength: 280,
     },
     createdAt: {
       type: Date,
-      default: Date.now()
+      default: Date.now(),
+      get: format,
     }
   }
-)
+);
+
+function setTwoDigits(num) {
+  let twoDigits = num.toString().padStart(2, '0');
+  return twoDigits;
+}
+
+function format(date) {
+  return (
+    [
+      setTwoDigits(date.getMonth() + 1), // because January returns 0
+      setTwoDigits(date.getDate()), // day
+      date.getFullYear(),
+    ].join('/') + ' ' + // put them together separated by a /
+    [
+      setTwoDigits(date.getHours()),
+      setTwoDigits(date.getMinutes()),
+      setTwoDigits(date.getSeconds()),
+    ].join(':') // separate them with a :
+  );
+}
