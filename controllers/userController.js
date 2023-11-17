68c63e10224bd9ffed22b5603e6f9605b58ca5e2
steven <smhanson21@gmail.com>
2023-11-17 16:22:36 -0600

    add populate friends and thoughts

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,9 @@ module.exports = {
   },
   async getSingleUser(req, res) {
     try {
-      const user = await User.findOne({ _id: req.params.userId });
+      const user = await User.findOne({ _id: req.params.userId })
+        .populate({ path: 'friends', select: '-__v' })
+        .populate({ path: 'thoughts', select: '-__v' });
 
       if (!user) {
         return res.status(404).json({ message: 'No user with that ID' });
