dcde895c45a1e6b02f3dca09ba59633d7e849f8e
steven <smhanson21@gmail.com>
2023-11-17 17:00:32 -0600

    add deleteUser logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Thought = require('../models/Thoughts');
 
 module.exports = {
   async getUsers(req, res) {
@@ -33,4 +34,19 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  // delete a user
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        res.status(404).json({ message: 'No user found with that ID' });
+      }
+
+      await Thought.deleteMany({ _id: { $in: user.thoughts } });
+      res.json({ message: 'User and thoughts deleted' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
 };
